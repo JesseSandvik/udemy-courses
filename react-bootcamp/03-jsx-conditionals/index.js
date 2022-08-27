5e45632cfa296509726a84a5692b84d5c61d31ea
Jesse Sandvik <jsandvik89@gmail.com>
2022-08-27 16:04:54 -0400

    created conditional component

---
+++ react-bootcamp/03-jsx-conditionals/index.js
@@ -0,0 +1,30 @@
+function getRandomNumber() {
+  return Math.floor(Math.random() * 10) + 1;
+}
+
+class NumberPicker extends React.Component {
+  render() {
+    const number = getRandomNumber();
+    let message;
+
+    if (number === 7) {
+      message = (
+        <div>
+          <h2>Congrats! You won!</h2>
+          <img src="https://media.giphy.com/media/l3nSQSlcI2cZvxsSQ/giphy.gif" />
+        </div>
+      );
+    } else {
+      message = <p>Sorry, you lose...</p>;
+    }
+
+    return (
+      <div>
+        <h1>Your number is {number}</h1>
+        {message}
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<NumberPicker />, document.getElementById("root"));

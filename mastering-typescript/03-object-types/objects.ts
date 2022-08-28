65cba446f07f104567dd06effe5bd1cf75c64214
Jesse Sandvik <jsandvik89@gmail.com>
2022-08-28 13:53:03 -0400

    covered type alias, a reusable reference to a type structure, primarily used for objects

--- mastering-typescript/03-object-types/objects.ts
+++ mastering-typescript/03-object-types/objects.ts
@@ -3,3 +3,29 @@ function printName(person: { first: string, last: string }): void {
 }
 
 printName({first: "Thomas", last: "Jane"});
+
+// let coordinates: { x: number; y: number; } = { x: 32, y: 15 };
+
+// function randomCoordinates(): {x: number, y:number } {
+//     return { x: Math.random(), y: Math.random() };
+// }
+
+// inline causes an error
+printName({ first: "Mick", last: "Jagger", age: 473 });
+
+const singer = { first: "Mick", last: "Jagger", age: 473 };
+
+// passing an object will not trigger this error (weird)
+printName(singer);
+
+// creates a reusable reference to a type "structure"
+type Point = {
+    x: number;
+    y: number;
+};
+
+let coordinates: Point = { x: 32, y: 15 };
+
+function randomCoordinates(): Point {
+    return { x: Math.random(), y: Math.random() };
+}

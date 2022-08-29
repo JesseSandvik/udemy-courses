67aa4e811220a75afe49f336dbd01545b9170654
Jesse Sandvik <jsandvik89@gmail.com>
2022-08-29 18:36:22 -0400

    added custom types to array elements

--- mastering-typescript/04-array-types/arrays.ts
+++ mastering-typescript/04-array-types/arrays.ts
@@ -6,3 +6,12 @@ const otherActiveUsers: Array<string> = [];
 otherActiveUsers.push("Tommy");
 // otherActiveUsers.push(4);
 
+type Point = {
+    x: number,
+    y: number,
+}
+
+const coordinates: Point[] = [];
+coordinates.push({x: 24, y: 8});
+// coordinates.push({x});
+// coordinates.push("hello");

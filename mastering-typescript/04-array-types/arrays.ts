27edb43ed0c5459b7510966ea600ebac449e1212
Jesse Sandvik <jsandvik89@gmail.com>
2022-08-29 18:31:29 -0400

    array types section added, first array types added to arrays file

---
+++ mastering-typescript/04-array-types/arrays.ts
@@ -0,0 +1,8 @@
+const activeUsers: string[] = [];
+activeUsers.push("Steven");
+// activeUsers.push(false);
+
+const otherActiveUsers: Array<string> = [];
+otherActiveUsers.push("Tommy");
+// otherActiveUsers.push(4);
+

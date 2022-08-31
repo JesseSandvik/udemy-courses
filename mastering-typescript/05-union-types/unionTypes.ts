e2a9a168d462223c3170a1f43320aedd7c35215c
Jesse Sandvik <jsandvik89@gmail.com>
2022-08-30 21:21:57 -0400

    expanded on unions in typescript, covering functions

--- mastering-typescript/05-union-types/unionTypes.ts
+++ mastering-typescript/05-union-types/unionTypes.ts
@@ -15,3 +15,20 @@ type Loc = {
 
 let coorinates: Point | Loc = { x: 13, y: 14 };
 coorinates = { lat: 44, long: 55 };
+
+function printAge(age: number | string): void {
+    console.log(`You are ${age} years old.`);
+}
+
+printAge(24);
+printAge("65");
+
+function calculatePrice(price: number | string, tax: number): number {
+    // return price * tax;
+    // price.replace("$", "");
+    if (typeof price === "string") {
+        price = price.replace("$", "");
+        price = parseFloat(price);
+    }
+    return price * tax;
+}

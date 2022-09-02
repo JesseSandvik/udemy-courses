568a83ead9bdc9828c8e3da73924ff10ddcd4119
Jesse Sandvik <jsandvik89@gmail.com>
2022-09-01 20:45:52 -0400

    began setState

--- react-bootcamp/11-state/src/App.js
+++ react-bootcamp/11-state/src/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from "react";
 import Game from "./Game";
 import OtherGame from "./OtherGame";
+import Rando from "./Rando";
 
 export default class App extends Component {
   render() {
     return (
       <div>
-        <Game />
-        <OtherGame />
+        <Rando maxNum={7} />
       </div>
     );
   }

dc5057ab3b0bc186e88f09d532bdf612b3873fd9
Jesse Sandvik <jsandvik89@gmail.com>
2022-09-03 12:02:01 -0400

    state exercise in react including conditional rendering

--- react-bootcamp/11-state/src/App.js
+++ react-bootcamp/11-state/src/App.js
@@ -5,12 +5,13 @@ import BrokenButton2 from "./BrokenButton2";
 import Game from "./Game";
 import OtherGame from "./OtherGame";
 import Rando from "./Rando";
+import Clicker from "./Clicker";
 
 export default class App extends Component {
   render() {
     return (
       <div>
-        <BrokenButton2 />
+        <Clicker />
       </div>
     );
   }
---
+++ react-bootcamp/11-state/src/Clicker.jsx
@@ -0,0 +1,28 @@
+import React, { Component } from 'react'
+
+export default class Clicker extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            number: 0,
+        };
+        this.getRandomNumber = this.getRandomNumber.bind(this);
+    }
+    getRandomNumber() {
+        const randomNumber = Math.floor(Math.random() * 10);
+        this.setState({ number: randomNumber });
+    }
+  render() {
+    return (
+      <div>
+        <h1>Clicker!</h1>
+        {this.state.number === 7 ? <h1>You win!</h1> : (
+            <div>
+            <button onClick={this.getRandomNumber}>Get Random Number</button>
+            <p>{this.state.number}</p>
+            </div>
+        )}
+      </div>
+    )
+  }
+}

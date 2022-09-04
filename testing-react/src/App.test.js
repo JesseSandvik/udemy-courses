e248e7091b9c449f251dd582577ea75cafdec4b3
Jesse Sandvik <jsandvik89@gmail.com>
2022-09-04 07:59:20 -0400

    TDD, wrote tests and assertions for color change of button, wrote React code to pass tests

--- testing-react/src/App.test.js
+++ testing-react/src/App.test.js
@@ -1,11 +1,15 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import App from "./App";
 
-test("renders learn react link", () => {
+test("button has correct initial color", () => {
   render(<App />);
-  // const linkElement = screen.getByText(/learn react/i);
-  const linkElement = screen.getByRole("link", { name: /learn react/i });
-  expect(linkElement).toBeInTheDocument();
-  // expect(element.textContent).toBe('hello');
-  // expect(elementsArray).toHaveLength(7);
+  const colorButton = screen.getByRole("button", { name: /change to blue/i });
+  expect(colorButton).toHaveStyle({ backgroundColor: "red" });
+
+  fireEvent.click(colorButton);
+
+  expect(colorButton).toHaveStyle({ backgroundColor: "blue" });
+  expect(colorButton.textContent.toLowerCase()).toBe("change to red");
 });
+// the above test does this already, not necessary
+// test("button has correct initial text", () => {});

1d60b296b39cb98ef8f1ee8b17588c0cba82b765
Jesse Sandvik <jsandvik89@gmail.com>
2022-09-04 07:29:27 -0400

    updated initial CRA test for React Testing Library best practices

--- testing-react/src/App.test.js
+++ testing-react/src/App.test.js
@@ -3,7 +3,8 @@ import App from "./App";
 
 test("renders learn react link", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  // const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByRole("link", { name: /learn react/i });
   expect(linkElement).toBeInTheDocument();
   // expect(element.textContent).toBe('hello');
   // expect(elementsArray).toHaveLength(7);

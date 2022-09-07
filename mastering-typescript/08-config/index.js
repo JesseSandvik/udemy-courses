eb6488327638193c76eed31ad2886ae6f1e1d832
Jesse Sandvik <jsandvik89@gmail.com>
2022-09-07 18:18:54 -0400

    config section for mastering-typescript created, complied first code

---
+++ mastering-typescript/08-config/index.js
@@ -0,0 +1,11 @@
+"use strict";
+const norma = {
+    breed: "Silkie",
+    eggsPerWeek: 4,
+    name: "Norma",
+};
+const barold = {
+    breed: "Silkie",
+    eggsPerWeek: 6,
+    name: "Barold",
+};
---
+++ mastering-typescript/08-config/index.ts
@@ -0,0 +1,17 @@
+interface Chicken {
+    breed: string;
+    eggsPerWeek: number;
+    name: string;
+}
+
+const norma: Chicken = {
+    breed: "Silkie",
+    eggsPerWeek: 4,
+    name: "Norma",
+};
+
+const barold: Chicken = {
+    breed: "Silkie",
+    eggsPerWeek: 6,
+    name: "Barold",
+};

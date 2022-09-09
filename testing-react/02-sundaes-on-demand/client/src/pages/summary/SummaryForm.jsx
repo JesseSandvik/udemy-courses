2f03bd56147602aef3eaf182a71eaffa14583870
Jesse Sandvik <jsandvik89@gmail.com>
2022-09-08 20:12:40 -0400

    built SummaryForm component to satisfy tests, all tests passing

--- testing-react/02-sundaes-on-demand/client/src/pages/summary/SummaryForm.jsx
+++ testing-react/02-sundaes-on-demand/client/src/pages/summary/SummaryForm.jsx
@@ -1,8 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react';
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
 
 function SummaryForm() {
+  const [tcChecked, setTcChecked] = useState(false);
+
+  const checkboxLabel = (
+    <span>
+      I agree to <span style={{ color: 'blue' }}>Terms and Conditions</span>
+    </span>
+  );
   return (
-    <div>SummaryForm</div>
+    <Form>
+      <Form.Group controlId='terms-and-condition'>
+        <Form.Check
+          type="checkbox"
+          checked={tcChecked}
+          onChange={(event) => setTcChecked(event.target.checked)}
+          label={checkboxLabel}
+        />
+      </Form.Group>
+      <Button variant='primary' type="submit" disabled={!tcChecked}>
+        Confirm Order
+      </Button>
+    </Form>
   )
 }
 

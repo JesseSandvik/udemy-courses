e6a3a9ca3959a8592a2d21632da9447b98d61516
Jesse Sandvik <jsandvik89@gmail.com>
2022-09-08 21:02:41 -0400

    updated tests to handle async removal of element from DOM

--- testing-react/02-sundaes-on-demand/client/src/pages/summary/test/SummaryForm.test.jsx
+++ testing-react/02-sundaes-on-demand/client/src/pages/summary/test/SummaryForm.test.jsx
@@ -1,4 +1,5 @@
-import { render, screen, fireEvent } from "@testing-library/react";
+import { render, screen, waitForElementToBeRemoved } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import SummaryForm from "../SummaryForm";
 
 test("Checkbox is unchecked by default", () => {
@@ -16,11 +17,28 @@ test("Checking the checkbox enables button, Unchecking checkbox again disables b
     const checkbox = screen.getByRole("checkbox", { name: /terms and conditions/i });
     const button = screen.getByRole("button", { name: /confirm order/i });
 
-    fireEvent.click(checkbox);
+    userEvent.click(checkbox);
 
     expect(button).toBeEnabled();
 
-    fireEvent.click(checkbox);
+    userEvent.click(checkbox);
 
     expect(button).toBeDisabled();
+});
+test("popover responds to hover", async () => {
+    render(<SummaryForm />);
+    // popover starts out hidden
+    const nullPopover = screen.queryByText(/No ice cream will actually be delivered/i);
+    expect(nullPopover).not.toBeInTheDocument();
+    // popover appears when we mouse over the checkbox label
+    const termsAndConditions = screen.getByText(/terms and conditions/i);
+
+    userEvent.hover(termsAndConditions);
+
+    const popover = screen.getByText(/No ice cream will actually be delivered/i);
+
+    expect(popover).toBeInTheDocument();
+    // popover disappears when we mouse out
+    userEvent.unhover(termsAndConditions);
+    await waitForElementToBeRemoved(() => screen.queryByText(/No ice cream will actually be delivered/i));
 });

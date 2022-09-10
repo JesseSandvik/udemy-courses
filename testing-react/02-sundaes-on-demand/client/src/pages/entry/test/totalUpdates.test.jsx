f377cee92dda9a2f28bc0fc8ef02603ae6ab01b0
Jesse Sandvik <jsandvik89@gmail.com>
2022-09-10 15:38:38 -0400

    context total update tests and assertions outlined in totalUpdates.test

---
+++ testing-react/02-sundaes-on-demand/client/src/pages/entry/test/totalUpdates.test.jsx
@@ -0,0 +1,22 @@
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import Options from "../Options";
+
+test("Update scoop subtotal when scoops change", async () => {
+    render(<Options optionType="scoops" />);
+
+    // make sure it starts at $0.00
+const scoopsSubtotal = screen.getByText("Scoops total: $", { exact: false });
+
+expect(scoopsSubtotal).toHaveTextContent("0.00");
+    // update vanilla scoops to 1 and check subtotal
+const vanillaInput = await screen.find("spinbutton", { name: "Vanilla" });
+userEvent.clear(vanillaInput);
+userEvent.type(vanillaInput, "1");
+expect(scoopsSubtotal).toHaveTextContent("2.00");
+    // update chocolate scoops to 2 and check subtotal
+const chocolateInput = await screen.find("spinbutton", { name: "Chocolate" });
+userEvent.clear(chocolateInput);
+userEvent.type(chocolateInput, "2");
+expect(scoopsSubtotal).toHaveTextContent("6.00");
+});

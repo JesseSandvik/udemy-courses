fa1f8e741719948e964162a64db2292205efd53c
Jesse Sandvik <jsandvik89@gmail.com>
2022-09-09 22:11:57 -0400

    setState practice to update pre-existing state and change state depending on previous state values

--- react-bootcamp/12-state-patterns/src/ScoreKeeper.jsx
+++ react-bootcamp/12-state-patterns/src/ScoreKeeper.jsx
@@ -3,17 +3,29 @@ import React, { Component } from 'react'
 class ScoreKeeper extends Component {
     constructor(props) {
         super(props);
-        this.state = { score: 0 };
+        this.state = { score: 0, count: 0 };
         this.singleKill = this.singleKill.bind(this);
+        this.tripleKill = this.tripleKill.bind(this);
     }
     singleKill() {
         this.setState({ score: this.state.score + 1 });
+    }
+    tripleKill() {
+        this.setState((state) => {return { score: state.score + 1 } });
+        this.setState((state) => {return { score: state.score + 1 } });
+        this.setState((state) => {return { score: state.score + 1 } });
+    }
+    incrementCounter(prevState) {
+        return { count: prevState.count + 3};
     }
   render() {
     return (
         <div>
             <h1>Score is: {this.state.score}</h1>
+            <h1>Count is: {this.state.count}</h1>
             <button onClick={this.singleKill}>Single kill!</button>
+            <button onClick={this.tripleKill}>Single kill!</button>
+            <button onClick={() => this.setState(this.incrementCounter)}>Single kill!</button>
         </div>
     )
   }

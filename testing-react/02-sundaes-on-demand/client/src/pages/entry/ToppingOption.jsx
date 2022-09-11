407de4750f1390a51688385886358519aad51cca
Jesse Sandvik <jsandvik89@gmail.com>
2022-09-11 12:21:28 -0400

    added code to satisfy tests

--- testing-react/02-sundaes-on-demand/client/src/pages/entry/ToppingOption.jsx
+++ testing-react/02-sundaes-on-demand/client/src/pages/entry/ToppingOption.jsx
@@ -1,10 +1,21 @@
 import Col from "react-bootstrap/Col";
+import Form from "react-bootstrap/Form";
 
-function ToppingOption({ name, imagePath }) {
+function ToppingOption({ name, imagePath, updateItemCount }) {
+  const handleChange = (event) => {
+    updateItemCount(name, event.target.Checked ? 1 : 0);
+  }
 
   return (
     <Col xs={12} sm={6} md={4} lg={3} style={{ textAlign: 'center' }}>
       <img style={{ width: '75%' }} alt={`${name} topping`} src={`http://localhost:3030/${imagePath}`} />
+      <Form.Group controlId={`${name}-topping-check`}>
+        <Form.Check
+          label={name}
+          type="checkbox"
+          onChange={handleChange}
+        />
+      </Form.Group>
     </Col>
   )
 }
--- testing-react/02-sundaes-on-demand/client/src/pages/entry/test/totalUpdates.test.jsx
+++ testing-react/02-sundaes-on-demand/client/src/pages/entry/test/totalUpdates.test.jsx
@@ -1,7 +1,6 @@
 import { fireEvent, render, screen } from "../../../test-utils/test-library-utils";
 import userEvent from "@testing-library/user-event";
 import Options from "../Options";
-import { OrderDetailsProvider } from "../../../contexts/OrderDetails";
 
 test("Update scoop subtotal when scoops change", async () => {
     render(<Options optionType="scoops" />);
@@ -23,23 +22,18 @@ expect(scoopsSubtotal).toHaveTextContent("6.00");
 });
 test("Update topping subtotal when toppings change", async () => {
     render(<Options optionType="toppings" />);
-    const toppingsSubtotal = screen.getByText("Toppings total: $", { exact: false });
 
+    const toppingsSubtotal = screen.getByText("Toppings total: $", { exact: false });
     expect(toppingsSubtotal).toHaveTextContent("0.00");
 
     const hotFudgeCheckbox = await screen.findByRole("checkbox", { name: /hot fudge/i });
-
     fireEvent.click(hotFudgeCheckbox);
-
     expect(toppingsSubtotal).toHaveTextContent("1.50");
 
     const peanutButterCupCheckboxes = await screen.findByRole("checkbox", { name: /peanut butter cups/i });
-
     fireEvent.click(peanutButterCupCheckboxes);
-
     expect(toppingsSubtotal).toHaveTextContent("3.00");
 
     fireEvent.click(hotFudgeCheckbox);
-
     expect(toppingsSubtotal).toHaveTextContent("1.50");
 });

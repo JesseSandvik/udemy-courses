752681e94ca7cbf62c1b215861c031f5a746b1fe
Jesse Sandvik <jsandvik89@gmail.com>
2022-09-11 12:06:14 -0400

    updated sundaes test to cover toppings subtotal changes

--- testing-react/02-sundaes-on-demand/client/src/pages/entry/ToppingOption.jsx
+++ testing-react/02-sundaes-on-demand/client/src/pages/entry/ToppingOption.jsx
@@ -1,6 +1,7 @@
 import Col from "react-bootstrap/Col";
 
 function ToppingOption({ name, imagePath }) {
+
   return (
     <Col xs={12} sm={6} md={4} lg={3} style={{ textAlign: 'center' }}>
       <img style={{ width: '75%' }} alt={`${name} topping`} src={`http://localhost:3030/${imagePath}`} />
--- testing-react/02-sundaes-on-demand/client/src/pages/entry/test/totalUpdates.test.jsx
+++ testing-react/02-sundaes-on-demand/client/src/pages/entry/test/totalUpdates.test.jsx
@@ -1,10 +1,10 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "../../../test-utils/test-library-utils";
 import userEvent from "@testing-library/user-event";
 import Options from "../Options";
 import { OrderDetailsProvider } from "../../../contexts/OrderDetails";
 
 test("Update scoop subtotal when scoops change", async () => {
-    render(<Options optionType="scoops" />, { wrapper: OrderDetailsProvider });
+    render(<Options optionType="scoops" />);
 
     // make sure it starts at $0.00
 const scoopsSubtotal = screen.getByText("Scoops total: $", { exact: false });
@@ -20,4 +20,26 @@ const chocolateInput = await screen.findByRole("spinbutton", { name: "Chocolate"
 userEvent.clear(chocolateInput);
 userEvent.type(chocolateInput, "2");
 expect(scoopsSubtotal).toHaveTextContent("6.00");
+});
+test("Update topping subtotal when toppings change", async () => {
+    render(<Options optionType="toppings" />);
+    const toppingsSubtotal = screen.getByText("Toppings total: $", { exact: false });
+
+    expect(toppingsSubtotal).toHaveTextContent("0.00");
+
+    const hotFudgeCheckbox = await screen.findByRole("checkbox", { name: /hot fudge/i });
+
+    fireEvent.click(hotFudgeCheckbox);
+
+    expect(toppingsSubtotal).toHaveTextContent("1.50");
+
+    const peanutButterCupCheckboxes = await screen.findByRole("checkbox", { name: /peanut butter cups/i });
+
+    fireEvent.click(peanutButterCupCheckboxes);
+
+    expect(toppingsSubtotal).toHaveTextContent("3.00");
+
+    fireEvent.click(hotFudgeCheckbox);
+
+    expect(toppingsSubtotal).toHaveTextContent("1.50");
 });

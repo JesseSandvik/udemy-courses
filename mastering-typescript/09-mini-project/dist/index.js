9d44505744d6da31d18ec4e6efd2f9bc31c744d8
Jesse Sandvik <jsandvik89@gmail.com>
2022-09-12 19:48:59 -0400

    DOM with TypeScript

--- mastering-typescript/09-mini-project/dist/index.js
+++ mastering-typescript/09-mini-project/dist/index.js
@@ -1,5 +1,9 @@
 "use strict";
 function printDouble(msg) {
-  console.log(msg);
-  console.log(msg);
+    console.log(msg);
+    console.log(msg);
 }
+printDouble("HELLO");
+// TypeScript already knows about the DOM and HTML elements
+const btn = document.querySelector('button');
+console.log(btn);
--- mastering-typescript/09-mini-project/src/index.ts
+++ mastering-typescript/09-mini-project/src/index.ts
@@ -2,3 +2,8 @@ function printDouble(msg: string): void {
     console.log(msg);
     console.log(msg);
 }
+
+printDouble("HELLO");
+// TypeScript already knows about the DOM and HTML elements
+const btn = document.querySelector('button');
+console.log(btn);

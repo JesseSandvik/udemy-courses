d007a2afbc9992e36642f3adc892366cf2886c5d
Jesse Sandvik <jsandvik89@gmail.com>
2022-09-12 19:37:46 -0400

    mini sample project, typescript

---
+++ mastering-typescript/09-mini-project/dist/index.js
@@ -0,0 +1,5 @@
+"use strict";
+function printDouble(msg) {
+  console.log(msg);
+  console.log(msg);
+}
---
+++ mastering-typescript/09-mini-project/src/index.ts
@@ -0,0 +1,4 @@
+function printDouble(msg: string): void {
+    console.log(msg);
+    console.log(msg);
+}

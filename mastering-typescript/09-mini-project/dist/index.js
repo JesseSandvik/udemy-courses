19daa64789bbe0e44bb5ce6a96a94f0234566cdf
Jesse Sandvik <jsandvik89@gmail.com>
2022-09-12 20:02:43 -0400

    non-null assertion operator, null check/assign if not-null

--- mastering-typescript/09-mini-project/dist/index.js
+++ mastering-typescript/09-mini-project/dist/index.js
@@ -5,6 +5,14 @@ function printDouble(msg) {
 }
 printDouble("HELLO");
 // TypeScript already knows about the DOM and HTML elements
-const btn = document.querySelector('button');
+const btn = document.getElementById("btn");
 console.log(btn);
 console.log("hello".replaceAll("l", "!"));
+btn === null || btn === void 0 ? void 0 : btn.addEventListener("click", function () {
+    alert("Clicked!");
+});
+// non-null assertion operator, only use this in situations where you know for 100% this will not be null
+const otherBtn = document.getElementById("btn");
+otherBtn.addEventListener("click", function () {
+    alert("Also clicked!");
+});
--- mastering-typescript/09-mini-project/src/index.ts
+++ mastering-typescript/09-mini-project/src/index.ts
@@ -5,7 +5,19 @@ function printDouble(msg: string): void {
 
 printDouble("HELLO");
 // TypeScript already knows about the DOM and HTML elements
-const btn = document.querySelector('button');
+const btn = document.getElementById("btn");
 console.log(btn);
 
 console.log("hello".replaceAll("l", "!"));
+
+btn?.addEventListener("click", function() {
+    alert("Clicked!");
+});
+
+// non-null assertion operator, only use this in situations where you know for 100% this will not be null
+
+const otherBtn = document.getElementById("btn")!;
+
+otherBtn.addEventListener("click", function() {
+    alert("Also clicked!");
+});

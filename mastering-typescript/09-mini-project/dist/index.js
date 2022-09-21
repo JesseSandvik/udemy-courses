37985657e0b6d9b74583c26a7f20e2698b7abb87
Jesse Sandvik <jsandvik89@gmail.com>
2022-09-21 19:13:01 -0400

    mini-project typescript with html elements

--- mastering-typescript/09-mini-project/dist/index.js
+++ mastering-typescript/09-mini-project/dist/index.js
@@ -1,24 +1,33 @@
 "use strict";
-function printDouble(msg) {
-    console.log(msg);
-    console.log(msg);
-}
-printDouble("HELLO");
-// TypeScript already knows about the DOM and HTML elements
-const btn = document.getElementById("btn");
-console.log(btn);
-console.log("hello".replaceAll("l", "!"));
+// function printDouble(msg: string): void {
+//     console.log(msg);
+//     console.log(msg);
+// }
+// printDouble("HELLO");
+// // TypeScript already knows about the DOM and HTML elements
+// const btn = document.getElementById("btn");
+// console.log(btn);
+// console.log("hello".replaceAll("l", "!"));
+// // btn?.addEventListener("click", function() {
+// //     alert("Clicked!");
+// // });
+// // non-null assertion operator, only use this in situations where you know for 100% this will not be null
+// // const otherBtn = document.getElementById("btn")!;
+// // otherBtn.addEventListener("click", function() {
+// //     alert("Also clicked!");
+// // });
+// // we can insist that this exists
+// const input = document.getElementById("todo-input")! as HTMLInputElement;
+// // this doesn't work with React/JSX, less commonly used anyway but same result
+// // (<HTMLInputElement>input).value;
 // btn?.addEventListener("click", function() {
-//     alert("Clicked!");
-// });
-// non-null assertion operator, only use this in situations where you know for 100% this will not be null
-// const otherBtn = document.getElementById("btn")!;
-// otherBtn.addEventListener("click", function() {
-//     alert("Also clicked!");
-// });
-// we can insist that this exists
+//     alert(input.value);
+//     input.value = "";
+// })
+const btn = document.getElementById("btn");
 const input = document.getElementById("todo-input");
-btn === null || btn === void 0 ? void 0 : btn.addEventListener("click", function () {
-    alert(input.value);
-    input.value = "";
+const form = document.querySelector("form");
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    console.log("submitted");
 });
--- mastering-typescript/09-mini-project/src/index.ts
+++ mastering-typescript/09-mini-project/src/index.ts
@@ -1,33 +1,42 @@
-function printDouble(msg: string): void {
-    console.log(msg);
-    console.log(msg);
-}
+// function printDouble(msg: string): void {
+//     console.log(msg);
+//     console.log(msg);
+// }
 
-printDouble("HELLO");
-// TypeScript already knows about the DOM and HTML elements
-const btn = document.getElementById("btn");
-console.log(btn);
+// printDouble("HELLO");
+// // TypeScript already knows about the DOM and HTML elements
+// const btn = document.getElementById("btn");
+// console.log(btn);
 
-console.log("hello".replaceAll("l", "!"));
+// console.log("hello".replaceAll("l", "!"));
+
+// // btn?.addEventListener("click", function() {
+// //     alert("Clicked!");
+// // });
+
+// // non-null assertion operator, only use this in situations where you know for 100% this will not be null
+
+// // const otherBtn = document.getElementById("btn")!;
+
+// // otherBtn.addEventListener("click", function() {
+// //     alert("Also clicked!");
+// // });
+// // we can insist that this exists
+// const input = document.getElementById("todo-input")! as HTMLInputElement;
+
+// // this doesn't work with React/JSX, less commonly used anyway but same result
+// // (<HTMLInputElement>input).value;
 
 // btn?.addEventListener("click", function() {
-//     alert("Clicked!");
-// });
+//     alert(input.value);
+//     input.value = "";
+// })
 
-// non-null assertion operator, only use this in situations where you know for 100% this will not be null
-
-// const otherBtn = document.getElementById("btn")!;
-
-// otherBtn.addEventListener("click", function() {
-//     alert("Also clicked!");
-// });
-// we can insist that this exists
+const btn = document.getElementById("btn")! as HTMLButtonElement;
 const input = document.getElementById("todo-input")! as HTMLInputElement;
+const form = document.querySelector("form")!;
 
-// this doesn't work with React/JSX, less commonly used anyway but same result
-// (<HTMLInputElement>input).value;
-
-btn?.addEventListener("click", function() {
-    alert(input.value);
-    input.value = "";
-})
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    console.log("submitted");
+});
